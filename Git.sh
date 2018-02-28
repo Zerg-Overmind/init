114626df57a7a8ede567a6d9ad5aad1e7b8d653e
ZergOvermind <563041763@qq.com>
2018-02-28 23:13:21 +0800

    new version

--- Git.sh
+++ Git.sh
@@ -2,8 +2,8 @@
 #A script to update the repository
 
 git status
-options=[Add/Commit/Diff/Exit/Log/Push/User/Reset/Status]
-while echo -e -n "\n\nPlease choose the first letter of options.\n$options? "
+options="[Add|Commit|Diff|Fetch|Exit|Help|Log|Push|User|Reset|Status]"
+while echo -e -n "\n\n\033[36mPlease choose the first letter of options.\n$options? \033[0m "
 	read -n1 choice
 do
 	echo -e "\n"
@@ -12,37 +12,65 @@ do
 		git add -A
 		git status;;
 	C | c)
-		echo -e -n "Please enter the remark: "
+		echo -e -n "\033[36mPlease enter the remark: \033[0m "
 		read commit
 		commitDate="[20"$(date +%y-%m-%d])
 		commit=$commitDate$commit
-		echo $commit
 		git commit -m "$commit";;
 	D | d)
 		git diff HEAD -- *;;
 	E | e)
 		exit 0;;
+	F | f)
+		git fetch origin master:origin_master
+		git diff origin_master
+		echo -e -n "\033[36mAre you sure to merge with origin_master[Y/N] \033[0m "
+		read -n1 merge_choice
+		echo -e "\n"
+		if [[ $merge_choice == 'y' ]] || [[ $merge_choice == 'Y' ]]; then
+			git merge origin_master
+		else
+			echo -e -n "\033[36mHaved canceled the merge option.\n \033[0m "
+		fi
+		git branch -D origin_master;;
+	H | h)
+		echo "	A: Add all changes to repository. "
+		echo "	C: Commit all changes to repository. "
+		echo "	D: Show differences between current version with HEAD->. "
+		echo "	E: Exit shell script. "
+		echo "	F: Fetch origin/master and merge. "
+		echo "	L: Show latest two-weeks logs of repository. "
+		echo "	P: Push commissions to origin/master. "
+		echo "	U: User command mode(Press ‘Enter’ to exit). "
+		echo "	R: Reset current version according version_id. "
+		echo "	S: Show status of repository. ";;
 	L | l)
-		echo "It only shows the logs for nearly two weeks."
+		echo -e -n "\033[36mIt only shows the logs for nearly two weeks. \033[0m "
 		git log --since=2.weeks --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit;;
 	P | p)
-	        git push origin master;;
+	        git push origin master
+			git status;;
 	U | u)
-		echo "Please input user's instructions."
-		echo -e -n ">> "
-		read myCmd
-		$myCmd;;
+		echo -e "\033[36mPlease input user's instructions. \033[0m "
+		while read -p ">> " myCmd
+		do
+			echo $myCmd
+			if [[ $myCmd != '' ]]; then
+				$myCmd
+			else
+				break
+			fi
+		done;;
 	R | r)
 		git log --since=2.weeks --color --graph --pretty=oneline
-		read -p "Please enter the reset_id: " reset_id
+		echo -e -n "\033[36mPlease enter the reset_id: \033[0m "
+		read reset_id
 		git reset --hard $reset_id;;
 	S | s)
 		git status;;
 	*)
 		echo
-		echo "Error input"
+		echo -e -n "\033[31m	Error input.\033[0m"
 		echo;;
 	esac
 done
-
-
